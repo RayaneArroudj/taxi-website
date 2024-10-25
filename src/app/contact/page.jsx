b7b6297403dca77c4e948911bc7d0e1c30f3a8c7
Rayane <rayane.arroudj@gmail.com>
2024-10-25 23:48:38 +0200

    contact form ok

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,26 +1,22 @@
-<<<<<<< HEAD
-export default function Contact() {
-  return <div>Contact</div>;
-}
-=======
 // app/contact/page.jsx
-import React from 'react'
-import ContactForm from '../../../email/form-contact'
+import React from "react";
+import ContactForm from "../../../email/form-contact";
 
 const ContactPage = () => {
   return (
     <div className="container mx-auto px-4 py-8">
       <h1 className="text-4xl font-bold mb-6 text-center">Contactez-nous</h1>
-      <p className='
-      text-center text-gray-400 mb-8 text-lg'>
+      <p
+        className="
+      text-center text-gray-400 mb-8 text-lg"
+      >
         Elite Lyon Taxi sera ravi de repondre à vos demandes.
       </p>
       <div className="max-w-2xl mx-auto">
         <ContactForm />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ContactPage
->>>>>>> axel
+export default ContactPage;
